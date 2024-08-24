4e0124ea9e30050de0e011aabcf62a0bd46ef160
theekko <theekkogames@gmail.com>
2024-08-24 13:28:54 -0700

    firing transform based arrow

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -4,14 +4,42 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour {
 
-    private Vector3 shootDir;
+    [SerializeField] private int damage = 1;
+    [SerializeField] private Vector2 knockback = Vector2.zero;
+    [SerializeField] private float destroyTimer = 2f;
     [SerializeField] float moveSpeed = 1f;
+
+    private Vector3 shootDir;
 
     public void Setup(Vector3 shootDir) {
         this.shootDir = shootDir;
+        // Calculate the angle in degrees for the rotation
+        float angle = Mathf.Atan2(shootDir.y, shootDir.x) * Mathf.Rad2Deg;
+
+        // Rotate the arrow to face the shoot direction
+        transform.eulerAngles = new Vector3(0, 0, angle);
+        Destroy(gameObject, destroyTimer);
     }
 
     private void Update() {
         transform.position += shootDir * moveSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Damageable damageable = collision.GetComponent<Damageable>();
+
+        if (damageable != null) {
+
+            Vector2 deliveredKnockback = transform.localScale.x > 0 ? knockback : new Vector2(-knockback.x, knockback.y);
+
+            bool gotHit = damageable.Hit(damage, deliveredKnockback);
+
+            if (gotHit) {
+                Debug.Log("Hit");
+                Destroy(gameObject);
+            }
+        } else {
+            Destroy(gameObject);
+        }
     }
 }
